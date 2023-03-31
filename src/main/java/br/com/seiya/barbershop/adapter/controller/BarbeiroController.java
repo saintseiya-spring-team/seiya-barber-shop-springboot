9c79efdfda71a5ac4696c66efd98fdf0ac8417b2
olivierpironi <pironiolivier@gmail.com>
2023-03-31 16:57:45 -0300

    Controller refatorado

--- src/main/java/br/com/seiya/barbershop/adapter/controller/BarbeiroController.java
+++ src/main/java/br/com/seiya/barbershop/adapter/controller/BarbeiroController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.seiya.barbershop.domain.dtos.Barbeiro;
@@ -22,7 +23,8 @@ import br.com.seiya.barbershop.domain.dtos.BarbeiroResponse;
 import br.com.seiya.barbershop.domain.ports.BarbeiroServicePort;
 import lombok.RequiredArgsConstructor;
 
-@RestController("/barbeiros")
+@RestController
+@RequestMapping("/barbeiros")
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class BarbeiroController {
